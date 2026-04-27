e75c730a67d3d3279b95cb9febd108ec3da4d8d5
Denis Vtulkin <daw81bk@gmail.com>
2026-04-27 18:19:56 +0200

    versionCode = 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.playeverywhere999.skip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "4"
+        versionCode = 5
+        versionName = "5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
