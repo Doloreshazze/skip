ec28810506a645bcdb411b09ad2c4d91fb61e88c
Doloreshazze <daw81bk@gmail.com>
2026-05-10 08:14:12 +0200

    versionCode = 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.playeverywhere999.skip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "7"
+        versionCode = 8
+        versionName = "8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
