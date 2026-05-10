2a3c7e70f7eb1da01a426708102addf04bae8167
Doloreshazze <daw81bk@gmail.com>
2026-05-10 07:20:42 +0200

    Fix missing TextAlign import in MainActivity

--- app/src/main/java/com/playeverywhere999/skip/MainActivity.kt
+++ app/src/main/java/com/playeverywhere999/skip/MainActivity.kt
@@ -81,6 +81,7 @@ import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.window.Dialog
