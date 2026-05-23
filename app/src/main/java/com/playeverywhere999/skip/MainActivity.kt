6c3bce12e487cbe56fe4620a0b6736f86e295439
Doloreshazze <daw81bk@gmail.com>
2026-05-23 13:07:15 +0200

    Use system bar insets to avoid stale keyboard gap

--- app/src/main/java/com/playeverywhere999/skip/MainActivity.kt
+++ app/src/main/java/com/playeverywhere999/skip/MainActivity.kt
@@ -36,7 +36,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -219,7 +219,7 @@ private fun AutoClickScreen() {
     Surface(
         modifier = Modifier
             .fillMaxSize()
-            .windowInsetsPadding(WindowInsets.safeDrawing)
+            .windowInsetsPadding(WindowInsets.systemBars)
             .background(gradientBackground)
     ) {
         if (!accessibilityEnabled) {
